4ef47c518e740929d9e01542826cbb2c7632041b
DanizitaDs <162368805+DanizitaDs@users.noreply.github.com>
2024-03-08 17:10:12 -0300

    Add files via upload

---
+++ DANI/aula03.js
@@ -0,0 +1,50 @@
+/*console.log('olah pessoal');
+console.log('ola mundo')
+//este codigo imprime uma mensagem
+const nome = 'danielle yasmin dias padilha'
+console.log('ola', nome)
+console.log('tudo bem?' , nome)
+const nomeUsuario = 'Danizita';
+const idade = 89
+console.log('voce é perfeita?' , nomeUsuario)
+console.log('obvio')
+console.log(idade)
+console.log(nomeUsuario , idade)
+console.log('meu nome é ' , nomeUsuario , 'e tenho' , idade , "anos, sabia??")
+const altura = 1.64
+const temperatura = 74
+console.log('hoje eu acordei e estava com' , temperatura , 'de temperatura!! muita loucura né' , nomeUsuario)
+let souUmBoolean = true
+souUmBoolean = false
+console.log
+*/
+
+const meuNome = 'Danielle';
+const meusobrenome = 'Padilha';
+const idade = 16;
+const estudante = true;
+
+console.log('meu nome é', meuNome, meusobrenome, 'e tenho' , idade, 'anos',',sou estudante??', estudante);
+
+const serie = 'bom dia,veronica'
+const temporada = 3
+const blue = true
+console.log(typeof serie)
+console.log(typeof temporada)
+console.log(typeof blue)
+console.log(typeof serie, typeof temporada, typeof blue)
+//este comando imprime o tipo de valor
+//pode ser string, number, boolean
+
+console.log('minha serie favorita é' , serie, 'tem' , temporada,'temporadas' ,'e ela é uma variavel' , typeof serie, 'e' , typeof temporada )
+
+const idade1 = 25
+const idadeTexto = idade.toString()
+
+console.log(typeof idade1)
+console.log(typeof idadeTexto)
+const idadeTexto2 = '23'
+const idadeNumero2 = Number(idadeTexto2)
+console.log(typeof idadeTexto2)
+console.log(typeof idadeNumero2)
+
