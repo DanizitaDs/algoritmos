ba4ddc00646d54d6ac3b6132089943539a83b901
DanizitaDs <162368805+DanizitaDs@users.noreply.github.com>
2024-03-12 17:23:02 -0300

    Add files via upload

---
+++ exercicio02resoolvido/resolucao.js
@@ -0,0 +1,26 @@
+//questão numero 1:
+let numero1 = 34;
+let numero2 = 24;
+let soma = numero1 + numero2
+console.log(soma)
+//questão numero 2:
+const nota1 = 20
+const nota2 = 30
+const nota3 = 40
+const dividi = 3
+const soma2 = nota1 + nota2 + nota3 
+const divisa = soma2/dividi 
+console.log(divisa)
+//questão numero 3:
+const c = 30
+const f = c * 9/5 + 32
+console.log(f)
+
+//questão numero 4:
+
+const base = 4
+const altura = 4
+const area = 4 * 4
+console.log(area)
+
+//questão numero 5:
