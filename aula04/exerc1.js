9e785931ba51f0f05163115a0b1cc950d0d16ac2
DanizitaDs <162368805+DanizitaDs@users.noreply.github.com>
2024-03-12 16:18:43 -0300

    Add files via upload

---
+++ aula04/exerc1.js
@@ -0,0 +1,14 @@
+//1- Rp: ( a= 10, b= 5 )
+//2- Rp: ( 10, 10, 10)
+
+let meuNome;
+let minhaIdade;
+
+console.log(typeof meuNome, typeof minhaIdade);
+
+// não funcionou pq não tinha um valor na variavel, no caso fica indefinido
+
+meuNome = 'danielle';
+minhaIdade = 16;
+
+console.log('qual é o seu nome?', '- meu nome é', meuNome , '/ qual a sua idade?', '- minha idade é' , minhaIdade , 'anos');
