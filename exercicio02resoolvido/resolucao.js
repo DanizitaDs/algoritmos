fc22508923b60c4c91538f88044d23f1d134829c
DanizitaDs <162368805+DanizitaDs@users.noreply.github.com>
2024-03-13 13:53:25 -0300

    Update resolucao.js

--- exercicio02resoolvido/resolucao.js
+++ exercicio02resoolvido/resolucao.js
@@ -1,9 +1,12 @@
 //questão numero 1:
+
 let numero1 = 34;
 let numero2 = 24;
 let soma = numero1 + numero2
 console.log(soma)
+
 //questão numero 2:
+
 const nota1 = 20
 const nota2 = 30
 const nota3 = 40
@@ -11,7 +14,9 @@ const dividi = 3
 const soma2 = nota1 + nota2 + nota3 
 const divisa = soma2/dividi 
 console.log(divisa)
+
 //questão numero 3:
+
 const c = 30
 const f = c * 9/5 + 32
 console.log(f)
@@ -21,6 +26,18 @@ console.log(f)
 const base = 4
 const altura = 4
 const area = 4 * 4
-console.log(area)
 
 //questão numero 5:
+
+const nomeUsuario = 'danielle '
+const sobreNome =  'padilha'
+const nomeCompleto = nomeUsuario + sobreNome
+console.log(nomeCompleto)
+
+//questão numero 6:
+
+const numero4 = '50'
+const numero5 = '27'
+const numeroExato = numero4 + numero5
+console.log(numeroExato) //para o numero ser somado é necessario que ele seja um number, no caso, nesta forma ele é um string e para transformar em number é necessario usar esse metodo: Number(nome da variavel).
+console.log(area)
