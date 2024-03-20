958dcc45e4eb31793e2aa93c746cd33e613dadb1
DanizitaDs <162368805+DanizitaDs@users.noreply.github.com>
2024-03-20 17:26:51 -0300

    Add files via upload

---
+++ exer4.js
@@ -0,0 +1,17 @@
+const readline = require(`readline`);
+
+const rl = readline.createInterface({
+input: process.stdin,
+output: process.stdout
+});
+
+rl.question('Digite o primeiro numero: ', (numero1) =>{
+    rl.question('Digite o segundo numero:',(numero2) => {
+})
+      if (numero1 * numero2)
+      console.log(resultado = numero1 * numero2)
+    if else
+      rl.close(); 
+    });
+        
+
